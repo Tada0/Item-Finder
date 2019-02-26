078f6222087832dc58ee12a6a907ece85e4f6933
Tomek <tomekholda@gmail.com>
2019-02-26 15:43:19 +0100

    Parent class for Scrappers Added

---
+++ src/WebsitesScrappers/Scrapper.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class Scrapper:
+
+    @staticmethod
+    def __get_page(product: str, page_nr: int):
+        pass
+
+    @staticmethod
+    def __get_additional_pages_number(first_page: requests.models.Response):
+        pass
+
+    @staticmethod
+    def __get_item(raw_item: tuple):
+        pass
+
+    @staticmethod
+    def __get_products_from_page(page_source: requests.models.Response):
+        pass
+
+    @staticmethod
+    def __get_all_products(product: str, first_page: requests.models.Response, pages: int):
+        pass
+
+    @staticmethod
+    def get_product(product: str):
+        pass
