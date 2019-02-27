4bd1d3d2620c059aaa522c5758f379b54cd0e0e2
Tomek <tomekholda@gmail.com>
2019-02-27 21:40:57 +0100

    .travis.yml updated

--- src/Main/main.py
+++
@@ -1,7 +0,0 @@
-from src.WebsitesScrappers.AllegroScrapper import AllegroScrapper
-from src.WebsitesScrappers.OlxScrapper import OlxScrapper
-
-
-if __name__ == "__main__":
-    print(AllegroScrapper.get_product_offers("Philips 240v"))
-    print(OlxScrapper.get_product_offers("Philips 240v"))
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from src.WebsitesScrappers.AllegroScrapper import AllegroScrapper
+from src.WebsitesScrappers.OlxScrapper import OlxScrapper
+from src.FileHandler.FileHandler import FileHandler
+
+
+if __name__ == "__main__":
+    all_products = AllegroScrapper.get_product_offers("Philips 240v") + OlxScrapper.get_product_offers("Philips 240v")
+
+    new_products = FileHandler.check_with_existing_products('Resources/aul.txt', all_products)
+    FileHandler.add_new_products('Resources/aul.txt', new_products)
