0b01465b234351b2cde42ec0c5d2f42a256f01db
Tomek <tomekholda@gmail.com>
2019-02-27 21:44:01 +0100

    Adding new urls to file functionality added

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.FileHandler.FileHandler import FileHandler
 
 
 if __name__ == "__main__":
-    all_products = AllegroScrapper.get_product_offers("Philips 240v") + OlxScrapper.get_product_offers("Philips 240v")
+    all_products = OlxScrapper.get_product_offers("Philips 240v") + AllegroScrapper.get_product_offers("Philips 240v")
 
-    new_products = FileHandler.check_with_existing_products('Resources/aul.txt', all_products)
-    FileHandler.add_new_products('Resources/aul.txt', new_products)
+    new_products = FileHandler.check_with_existing_products('../Resources/aul.txt', all_products)
+    FileHandler.add_new_products('../Resources/aul.txt', new_products)
